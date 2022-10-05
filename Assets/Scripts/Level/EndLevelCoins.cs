0286b7d1ab6c864c467e0c402f0f003ee0fa1b7c
Sergio Damián Bressán <sergiodb95@gmail.com>
2022-10-05 00:00:07 -0300

    hecho un coso para obtener monedas a traves del tiempo, falta probar

---
+++ Assets/Scripts/Level/EndLevelCoins.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevelCoins : MonoBehaviour
+{
+    public int totalCoins = 0;
+    public int levelCoins = 0;
+    private const float crazyNumber = 4.4f;
+    public static EndLevelCoins instance {get; private set;}
+
+    public void GenerateCoinsEndLevel(float finishLine, float torque, float timer)
+    {
+        float minTime = 0;
+        float midTime = 0;
+        float maxTime = 0;
+
+        minTime = finishLine / 1 * crazyNumber;
+        midTime = finishLine / (torque / 2) * crazyNumber;
+        maxTime = finishLine / torque * crazyNumber;
+
+        int minCoin = 1;
+        int midCoin = 25;
+        int maxCoin = 100;
+        float differenceCoin = 0;
+        float differenceTimer = 0;
+        float numberCoinTimer = 0;
+
+        if (timer <= minTime && timer >= midTime)
+        {
+            differenceCoin = midCoin - minCoin;
+            differenceTimer = (minTime - midTime);
+            numberCoinTimer = differenceTimer / differenceCoin;
+
+            levelCoins = midCoin - (int)Mathf.Floor(timer/numberCoinTimer);
+        }
+        else if (timer <= midTime && timer >= maxTime)
+        {
+            differenceCoin = maxCoin - midCoin;
+            differenceTimer = (midTime - maxTime);
+            numberCoinTimer = differenceTimer / differenceCoin;
+
+            levelCoins = maxCoin - (int)Mathf.Floor(timer/numberCoinTimer);
+        }
+        else if (timer < maxTime)
+        {
+            levelCoins = minCoin;
+        }
+        else if (timer > minTime)
+        {
+            levelCoins = minCoin;
+        }
+
+    }
+}
