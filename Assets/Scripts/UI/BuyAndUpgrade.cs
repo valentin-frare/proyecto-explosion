658db53e751a937f91b6a54147c42e4dcdc9fe6c
Sergio Damián Bressán <sergiodb95@gmail.com>
2022-10-12 01:09:09 -0300

    comportamiento del boton de comprar de la estacion de servicio

--- Assets/Scripts/UI/BuyAndUpgrade.cs
+++ Assets/Scripts/UI/BuyAndUpgrade.cs
@@ -7,6 +7,8 @@ using TMPro;
 public class BuyAndUpgrade : MonoBehaviour
 {
     [SerializeField]
+    private GameObject textSt;
+    [SerializeField]
     private GameObject vehicles;
     [SerializeField]
     private GameObject upgrades;
@@ -19,6 +21,10 @@ public class BuyAndUpgrade : MonoBehaviour
     public List<VehicleConfig> scriptVeh = new List<VehicleConfig>();
 
     private VehicleConfig selectedVehicle;
+    private GameObject vehicleToBuy;
+    private int vehiclePrice;
+    private GameObject upgradeToBuy;
+    private int upgradePrice;
     
     void Start()
     {
@@ -34,6 +40,8 @@ public class BuyAndUpgrade : MonoBehaviour
     {
         if (GameManager.instance.gameState == GameState.Victory && go.GetComponent<Toggle>().isOn == true)
         {
+            vehicleToBuy = go;
+            vehiclePrice = int.Parse(go.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text);
             Transform up = upgrades.transform;
             foreach (Transform obj in up)
             {
@@ -47,6 +55,21 @@ public class BuyAndUpgrade : MonoBehaviour
             upgrades.transform.GetChild(0).GetChild(1).GetComponent<TextMeshProUGUI>().text = selectedVehicle.priceUpgradeSpeed.ToString();
             upgrades.transform.GetChild(1).GetChild(1).GetComponent<TextMeshProUGUI>().text = selectedVehicle.priceUpgradeEndurance.ToString();
             upgrades.transform.GetChild(2).GetChild(1).GetComponent<TextMeshProUGUI>().text = selectedVehicle.priceUpgradeHandLing.ToString();
+
+            if (int.Parse(go.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text) == 0)
+            {
+                foreach (Transform obj in up)
+                {
+                    obj.gameObject.GetComponent<Toggle>().interactable = true;
+                }
+            }
+            else
+            {
+                foreach (Transform obj in up)
+                {
+                    obj.gameObject.GetComponent<Toggle>().interactable = false;
+                }
+            }
         }
     }
 
@@ -54,6 +77,8 @@ public class BuyAndUpgrade : MonoBehaviour
     {
         if (GameManager.instance.gameState == GameState.Victory && go.GetComponent<Toggle>().isOn == true)
         {
+            upgradeToBuy = go;
+            upgradePrice = int.Parse(go.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text);
             switch (go.name)
             {
                 case "U0":
@@ -85,6 +110,46 @@ public class BuyAndUpgrade : MonoBehaviour
                     break;
                 default:
                     break;
+            }
+        }
+    }
+
+    public void BuyThings()
+    {
+        Transform up = upgrades.transform;
+
+        if (vehiclePrice > 0)
+        {
+            if (vehiclePrice <= EndLevelCoins.instance.totalCoins)
+            {
+                EndLevelCoins.instance.totalCoins -= vehiclePrice;
+                vehicleToBuy.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = 0.ToString();
+                textSt.GetComponent<TextMeshProUGUI>().text = EndLevelCoins.instance.totalCoins.ToString();
+                foreach (Transform obj in up)
+                {
+                    obj.gameObject.GetComponent<Toggle>().interactable = true;
+                }
+            }
+        }
+        else
+        {
+            int x = 0;
+            foreach (Transform obj in up)
+            {
+                if (obj.gameObject.GetComponent<Toggle>().isOn == true)
+                {
+                    x++;
+                }
+            }
+            if (x > 0)
+            {
+                if (upgradePrice <= EndLevelCoins.instance.totalCoins)
+                {
+                    EndLevelCoins.instance.totalCoins -= upgradePrice;
+                    upgradeToBuy.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = 0.ToString();
+                    textSt.GetComponent<TextMeshProUGUI>().text = EndLevelCoins.instance.totalCoins.ToString();
+                    upgradeToBuy.GetComponent<Toggle>().interactable = false;
+                }
             }
         }
     }
